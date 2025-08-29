7b5df3dcc18c8bc7dd3ed068ab621c2bf9ed16c9
Ivan Fontosh <ifontosh@it-aces.com>
2025-08-29 10:25:25 +0800

    fix null description

--- src/utils/clearDescription.ts
+++ src/utils/clearDescription.ts
@@ -1,4 +1,7 @@
-export const clearDescription = (s: string) => {
-    return s.replace(/<a\b[^>]*>(.*?)<\/a>/gi, "$1") // удаляем теги <a>, оставляя содержимое
-        .replace(/\[.*?\]/g, ""); // удаляем все что находится в квадратных скобках
-}
+export const clearDescription = (s: string | null) => {
+  if (!s) return "";
+
+  return s
+    .replace(/<a\b[^>]*>(.*?)<\/a>/gi, "$1") // удаляем теги <a>, оставляя содержимое
+    .replace(/\[.*?\]/g, ""); // удаляем все что находится в квадратных скобках
+};
